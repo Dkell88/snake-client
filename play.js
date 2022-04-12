71b02b2599bc46e80bba29031c759f03b20638f7
Dkell88 <DarrenKelly1188@gmail.com>
2022-04-12 16:17:39 -0700

    Adding initial version of client.js

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
 const { Console } = require("console");
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Connected!");
-  });
-  
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
