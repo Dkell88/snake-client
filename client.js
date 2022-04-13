27da66f28d99d667619a60ddde5df5cb30f78501
Dkell88 <DarrenKelly1188@gmail.com>
2022-04-13 13:59:11 -0700

    Added constant.js file

--- client.js
+++ client.js
@@ -1,11 +1,15 @@
 const { Console } = require("console");
 const net = require("net");
+const path = require("./constants");
+
+console.log(path.IP);
+console.log(path.port);
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: path.IP,
+    port: path.port
   });
 
   // interpret incoming data as text
@@ -17,24 +21,6 @@ const connect = function() {
 
   conn.on('connect', () => {
     
-    //Harcoded movements
-    // for (let i = 0; i < 360; i++) {
-  
-    //   setTimeout(() => {
-    //     let dir = 'Move: up';
-    //     console.log(i);
-    //     if (i <= 30) {
-    //       dir = 'Move: down';
-    //     } else if (i > 30 && i <= 60) {
-    //       dir = 'Move: left';
-    //     } else if (i > 60 && i <= 90) {
-    //       dir = 'Move: up';
-    //     } else if (i > 90 && i <= 120) {
-    //       dir = 'Move: right';
-    //     }
-    //     conn.write(dir);
-    //   }, (i * 50));
-    // }
   });
 
   conn.on('data', (data) => console.log('Server says: ', data));
--- input.js
+++ input.js
@@ -1,34 +1,36 @@
 let connection;
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
 
   if (key === '\u0003') {
     process.exit();
   }
   if (key === 'w') {
     connection.write('Move: up');
-  }
-  if (key === 'd') {
+  } else if (key === 'd') {
     connection.write('Move: right');
-  }
-  if (key === 's') {
+  } else if (key === 's') {
     connection.write('Move: down');
-  }
-  if (key === 'a') {
+  } else if (key === 'a') {
     connection.write('Move: left');
+  } else if (key === 'e') {
+    connection.write('Say: YEET!');
+  } else if (key === 'f') {
+    connection.write('Say: Ello');
+  } else if (key === 'x') {
+    connection.write('Say: get wrek\'d');
   }
-  console.log("key = ", key);
+
 };
 
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
-  console.log("setupInput entered");
+  //console.log("setupInput entered");
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
-
   return stdin;
 };
 module.exports = {setupInput};
