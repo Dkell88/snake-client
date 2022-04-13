9d720d8330f8eaa25e084921c193d110489a27fc
Dkell88 <DarrenKelly1188@gmail.com>
2022-04-13 09:58:01 -0700

    Adding initial version of input.js and moving setupInput function to input.js

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const { Console } = require("console");
 const net = require("net");
 const { connect } = require("./client");
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-
-};
-
-const setupInput = function () {
-  console.log("setupInput entered");
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const {setupInput} = require("./input");
 
 console.log("Connecting ...");
 connect();
